924e0cc2dacb41b6c2766ea2d36442ea83f4c01d
mohammad-mohamed <mohammad.mhm16@gmail.com>
2024-09-06 12:19:37 +0200

    added the code for route

--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import 'normalize.css'
+import './assets/css/style.css';
 
 createApp(App).use(store).use(router).mount('#app')
